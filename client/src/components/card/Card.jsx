f0857925b140465d7f02eb34bed708cb6870109a
pedro <pedro.bagarini2000@gmail.com>
2023-11-13 17:31:23 -0300

    Button deletado e alterações no home e card

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,36 +1,16 @@
-import { useState } from "react";
-import MovieDesc from "../movieDescription/movieDescription";
-import styles from "./Card.module.css";
+import styles from "./card.module.css";
 
-const MovieCard = ({ movies }) => {
-  const movie = movies;
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
+const Card = ({ children, img }) => {
+
   };
   return (
-    <>
-      <div className={styles.movie} onClick={toggleModal}>
-        <div>
-          <p>{movie.Year}</p>
-        </div>
-        <div>
-          <img
-            src={
-              movie.Poster !== "N/A"
-                ? movie.Poster
-                : "https://via.placeholder.com/400"
-            }
-            alt={movie.Title}
-          />
-        </div>
-        <div>
-          <span>{movie.Type}</span>
-          <h3>{movie.Title}</h3>
-        </div>
-      </div>
-      {isModalOpen && <MovieDesc movies={movie} click={toggleModal} />}
-    </>
+    <div className={styles.racao1}>
+      <img src={img} alt="rações para cães" width={"275"} />
+      <p>{children}</p>
+      <button className={styles.btn} onClick={alert2}>
+        Compre Agora
+      </button>
+    </div>
   );
-};
-export default MovieCard;
+
+export default Card;
